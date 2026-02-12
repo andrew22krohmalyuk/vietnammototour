1809c532abfb6ea3b0b7ea4e53ff19c85968c753
Andrew <andrew.krohmalyuk12@gmail.com>
2026-02-13 00:08:07 +0200

    setup deploy (#1)
    Co-authored-by: Andrew Krokhmaliuk <andrii.krokhmaliuk@virtru.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
+import packageJson from './package.json' with { type: 'json' };
+
 const nextConfig = {
   /* config options here */
+  output: 'export',
+  distDir: 'dist',
   reactStrictMode: true,
+  basePath: `/${packageJson.name}`,
 };
 
 export default nextConfig;
