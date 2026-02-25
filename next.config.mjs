a78fdce1f82300c0def09f56e74b4003ac859399
Andrew Krokhmaliuk <andrew.krohmalyuk12@gmail.com>
2026-02-25 14:03:20 +0200

    init payload cms

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,17 @@
+import { withPayload } from '@payloadcms/next/withPayload'
+
 /** @type {import('next').NextConfig} */
-import packageJson from './package.json' with { type: 'json' };
-
 const nextConfig = {
-  /* config options here */
-  // output: 'export',
-  // distDir: 'dist',
-  reactStrictMode: true,
-  // images: {
-  //   unoptimized: true,
-  // },
-};
+  // Your Next.js config here
+  webpack: (webpackConfig) => {
+    webpackConfig.resolve.extensionAlias = {
+      '.cjs': ['.cts', '.cjs'],
+      '.js': ['.ts', '.tsx', '.js', '.jsx'],
+      '.mjs': ['.mts', '.mjs'],
+    }
 
-export default nextConfig;
+    return webpackConfig
+  },
+}
+
+export default withPayload(nextConfig, { devBundleServerPackages: false })
